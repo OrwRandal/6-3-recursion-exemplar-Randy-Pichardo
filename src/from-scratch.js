770a47fe0d88ceb18e240d8ccd2bc99adb2c45a4
benspector-mls <ben@marcylabschool.org>
2025-02-13 15:26:10 -0500

    init

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,7 +1,18 @@
-const functionOne = () => {
+// Recursion Problem Set: Coding Exercises
 
-}
+// Calculate and return the total sum using recursive approach
+const sum = (arr) => {}
 
-module.exports = {
-  functionOne
-}
+// Reverse string using recursive approach
+const reverse = (str) => {};
+
+// Iterative approach to solving the fibonacci problem
+const fibIter = (n) => {};
+
+// Recursive fibonacci
+const fibRec = (n) => {};
+
+// Return the index of target in arr, or -1 if not found using recursion 
+const binarySearch = (arr, target, start = 0, end = arr.length - 1) => {};
+
+module.exports = { sum, reverse, fibRec, fibIter, binarySearch };
